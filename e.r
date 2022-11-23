b4f53e9741012ea80c6004cf7896036986260d8f
seirra blake <sophietheopossum@yandex.ru>
2022-11-23 15:57:44 +0000

    include useful note

--- e.r
+++ e.r
@@ -1,7 +1,7 @@
 # 3.1 | 95% confidence interval for a range of values
 balance <- c(983,1002,998,996,1002,983,994,991,1005,986)
 t.test(balance, conf.level = 0.95, mu = 360)
-# 3.2.i | calculate an estimator for the values
+# 3.2.i | calculate an estimator for the values (an estimator can include the mean)
 ratios <- c(5,7,9,10,14,23,20,15)
 ratiosMean <- mean(ratios)
 # 3.2.ii | 95% confidence interval for a range of values
