252f1ef8f5675ea4a351b4d606610b6bd0dfc2bd
seirra blake <sophietheopossum@yandex.ru>
2022-11-23 15:45:46 +0000

    improe separator style

--- e.r
+++ e.r
@@ -1,3 +1,3 @@
-# 3.1, 95% confidence interval for a range of values
+# 3.1 | 95% confidence interval for a range of values
 balance <- c(983,1002,998,996,1002,983,994,991,1005,986)
 t.test(balance, conf.level = 0.95, mu = 360)
