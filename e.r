43084b093e866106a6c91c17e5015f783948a4df
seirra blake <sophietheopossum@yandex.ru>
2022-11-30 16:08:40 +0000

    add normality tests

--- e.r
+++ e.r
@@ -1,5 +1,7 @@
 # 3.1 | 95% confidence interval for a range of values
 balance <- c(983,1002,998,996,1002,983,994,991,1005,986)
+shapiro.test(balance)
+ks.test(scale(balance), "pnorm")
 t.test(balance, mu = 1000)
 # 3.2.i | calculate an estimator for the values (an estimator can include the mean)
 ratios <- c(5,7,9,10,14,23,20,15)
@@ -10,6 +12,8 @@ t.test(ratios, mu = ratiosMean)
 t.test(ratios, conf.level = 0.99, mu = ratiosMean)
 # 3.4 | soap is usually mean price of 50 and standard deviation of 5. do convenience stores fit that trend?
 soaps <- c(45, 59, 58, 49, 55, 41, 66, 75, 39, 48)
+shapiro.test(soaps)
+ks.test(scale(soaps), "pnorm")
 soapsMean <- mean(soaps)
 soapsDeviat <- sd(soaps)
 # convenience stores have a higher mean price and a wider standard deviation
