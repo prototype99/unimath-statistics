faec8e6f6586a4a775593b52ae028488afe23e04
seirra blake <sophietheopossum@yandex.ru>
2022-11-30 15:17:36 +0000

    remove default values

--- e.r
+++ e.r
@@ -1,11 +1,11 @@
 # 3.1 | 95% confidence interval for a range of values
 balance <- c(983,1002,998,996,1002,983,994,991,1005,986)
-t.test(balance, conf.level = 0.95, mu = 1000)
+t.test(balance, mu = 1000)
 # 3.2.i | calculate an estimator for the values (an estimator can include the mean)
 ratios <- c(5,7,9,10,14,23,20,15)
 ratiosMean <- mean(ratios)
 # 3.2.ii | 95% confidence interval for a range of values
-t.test(ratios, conf.level = 0.95, mu = ratiosMean)
+t.test(ratios, mu = ratiosMean)
 # 3.2.iii | 99% confidence interval for a range of values
 t.test(ratios, conf.level = 0.99, mu = ratiosMean)
 # 3.4 | soap is usually mean price of 50 and standard deviation of 5. do convenience stores fit that trend?
