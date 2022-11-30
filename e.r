9fe9cec8dc0d938fb8fa1a2e77409de6c776e7b6
seirra blake <sophietheopossum@yandex.ru>
2022-11-30 15:15:47 +0000

    add more accurate hypothesis

--- e.r
+++ e.r
@@ -1,6 +1,6 @@
 # 3.1 | 95% confidence interval for a range of values
 balance <- c(983,1002,998,996,1002,983,994,991,1005,986)
-t.test(balance, conf.level = 0.95, mu = 360)
+t.test(balance, conf.level = 0.95, mu = 1000)
 # 3.2.i | calculate an estimator for the values (an estimator can include the mean)
 ratios <- c(5,7,9,10,14,23,20,15)
 ratiosMean <- mean(ratios)
